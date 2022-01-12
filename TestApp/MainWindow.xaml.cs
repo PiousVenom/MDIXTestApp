f1bd398724133c4ef4f49014fcbcda6a8decad7f
Kevin Hebert <khebert@cpat.com>
2022-01-12 15:22:36 -0600

    Test App Commit

---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace TestApp;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        AddItems();
+    }
+
+    private void AddItems()
+    {
+        CmbTest.Items.Add("Item 1");
+        CmbTest.Items.Add("Item 2");
+        CmbTest.Items.Add("Item 3");
+        CmbTest.Items.Add("Item 4");
+        CmbTest.Items.Add("Item 5");
+        CmbTest.Items.Add("Item 6");
+        CmbTest.Items.Add("Item 7");
+    }
+}
